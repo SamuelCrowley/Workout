b12ce4a2dda81589bfd6768abfe0f0b126b7352a
Sam <sam.crowley@sgdigital.com>
2025-05-04 18:25:42 -0400

    Bootstrap bug

--- Workout.Domain/obj/Release/net8.0/Workout.Domain.AssemblyInfo.cs
+++ Workout.Domain/obj/Release/net8.0/Workout.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workout.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b8c6cf3ac465a7440bfbad37cdc01212a7b905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290c762c7253a2398c41d2691c01204b3e25a6f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workout.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workout.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
